b98d6e84a137291b45d5cad4175653b79fa4bd22
Melissa Winstanley <mwinst@cs.washington.edu>
2012-07-14 20:55:06 -0700

    Fixed redis refresh session

--- controllers/profilecontroller.js
+++ controllers/profilecontroller.js
@@ -1,16 +1,22 @@
+var User = require('../models/usermodel.js');
+
 exports.profile = function(req, res) {
-  res.render('profile/index.jade', {
-    title: 'Your Profile',
-    firstname: 'John',
-    lastname: 'Doe',
-    position: 'Developer',
-    company: 'Amazon.com',
-    geoloc: 'Seattle, WA',
-    photoref: 'http://www.almostsavvy.com/wp-content/uploads/2011/04/profile-photo.jpg',
-    twitter: 'twitter.com/johndoe',
-    rating: '57',
-    strength1: 'Leadership',
-    strength2: 'Teamwork',
-    strength3: 'Networking'
+  console.log(req.param('_id'));
+  User.findOne({_id: req.param('_id')}, function(err, user) {
+    res.render('profile/index.jade', {
+      title: 'Your Profile',
+      firstname: user && user.firstname || 'John',
+      lastname: user && user.lastname || 'Doe',
+      position: 'Developer',
+      company: 'Amazon.com',
+      geoloc: 'Seattle, WA',
+      photoref: user && user.photoref || 'http://www.almostsavvy.com/wp-content/uploads/2011/04/profile-photo.jpg',
+      twitter: 'twitter.com/johndoe',
+      rating: '57',
+      strength1: 'Leadership',
+      strength2: 'Teamwork',
+      strength3: 'Networking'
     });
+                  
+  });
 } 
