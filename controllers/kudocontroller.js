2f413702c23fd68bfaf6cd855473eb39d6438651
Melissa Winstanley <mwinst@cs.washington.edu>
2012-07-15 16:38:52 -0700

    auto update photo

--- controllers/kudocontroller.js
+++ controllers/kudocontroller.js
@@ -120,7 +120,7 @@ exports.submitPost = function(req, res) {
         var cuser = User.findOne({_id: kudo.creator}, function(error, doc) {
           
           var message = '<div class="row"><div class="kudopicturespan">' +
-          '<img src="https://cacm.acm.org/system/assets/0000/7989/51812.bbcnews.ruchi_sanghvi_facebook.large.jpg?1341312421&1337358501" height="60px" width="60px" class="picture recommender_picture"></div>' +
+          '<img src="' + doc.photoref + '" height="60px" width="60px" class="picture recommender_picture"></div>' +
           '<div class="shoutoutspan" style="margin: 0 10 0 10; width: 90%; height:100%;"><div class="shoutout">' + kudo.content + '</div></div></div>'+
           '<div class="row" style="margin-left:50px"><div class="creator">' + doc.firstname + ' ' + doc.lastname[0].toUpperCase() + '.</div>'+
           '<div class="date">Sent July 15</div></div>';
