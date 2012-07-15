ae03a871647f2ae7e7b8a3a9f7d66e9b59a751a6
William Thurston <william@foozlegame.com>
2012-07-15 15:29:32 -0700

    sample code to grab 10 most recent kudos

--- models/kudomodel.js
+++ models/kudomodel.js
@@ -5,10 +5,10 @@ var KudoSchema = new Schema({
   content: {type: String, required: true},
   targetuser: {type: String, required: true},
   creator: {type: String, required: true},
-  date: {type: Date, required: true},
+  date: {type: Date, required: true, index: true},
   c_privacy: {type: Number},
   t_privacy: {type: Number},
-  tags: {type: Array}
+  tags: {type: Array, required: false}
 });
 
 module.exports = mongoose.model('Kudo', KudoSchema);
