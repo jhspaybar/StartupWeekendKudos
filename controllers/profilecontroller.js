ff9ba3fd503c2007c7e66dcd3301cb9c5f1dafc9
Elizabeth Chaddock <elchaddock@gmail.com>
2012-07-15 11:26:09 -0700

    Fixing date and sender

--- controllers/profilecontroller.js
+++ controllers/profilecontroller.js
@@ -10,15 +10,21 @@ exports.profile = function(req, res) {
       position: user && user.position || 'Developer',
       company: user && user.company || 'Amazon.com',
       geoloc: user && user.geoloc || 'Seattle, WA',
-      photoref: user && user.photoref || 'http://www.almostsavvy.com/wp-content/uploads/2011/04/profile-photo.jpg',
+      photoref: user && user.photoref || '//www.almostsavvy.com/wp-content/uploads/2011/04/profile-photo.jpg',
       twitter: 'twitter.com/johndoe',
       rating: '57',
       strength1: 'Leadership',
       strength2: 'Teamwork',
       strength3: 'Networking',
-      photoref_1: 'http://cacm.acm.org/system/assets/0000/7989/51812.bbcnews.ruchi_sanghvi_facebook.large.jpg?1341312421&1337358501',
-      kudo_text: 'Thanks for your help during Startup Weekend!'
-    });
+      photoref_1: '//cacm.acm.org/system/assets/0000/7989/51812.bbcnews.ruchi_sanghvi_facebook.large.jpg?1341312421&1337358501',
+      kudo_text: 'Thanks for your help during Startup Weekend! I had so much fun working with you!',
+      time_sent: '4:05 PM',
+      time_sent2: '12:30 PM',
+      time_sent3: '10:48 AM',
+      name_0: 'Naomi K.',
+      name_1: 'Liz C.',
+      name_2: 'Colleen M.' 
+   });
                   
   });
 };
