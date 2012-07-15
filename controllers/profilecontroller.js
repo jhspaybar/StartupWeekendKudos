026aa1e18be34d4003e9088d511ce6da116c5f31
Elizabeth Chaddock <elchaddock@gmail.com>
2012-07-14 21:21:48 -0700

    Updating profile at end of day

--- controllers/profilecontroller.js
+++ controllers/profilecontroller.js
@@ -1,8 +1,8 @@
 exports.profile = function(req, res) {
   res.render('profile/index.jade', {
     title: 'Your Profile',
-    firstname: 'John',
-    lastname: 'Doe',
+    firstname: 'Elizabeth',
+    lastname: 'Chaddock',
     position: 'Developer',
     company: 'Amazon.com',
     geoloc: 'Seattle, WA',
@@ -11,6 +11,8 @@ exports.profile = function(req, res) {
     rating: '57',
     strength1: 'Leadership',
     strength2: 'Teamwork',
-    strength3: 'Networking'
+    strength3: 'Networking',
+    photoref_1: 'http://cacm.acm.org/system/assets/0000/7989/51812.bbcnews.ruchi_sanghvi_facebook.large.jpg?1341312421&1337358501',
+    kudo_text: 'Thanks for your help during Startup Weekend!'
     });
 } 
